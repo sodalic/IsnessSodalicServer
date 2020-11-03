53e976374add5ec316e9c49ba25d9bd4dae3ecce
Joe Hardy <jlharyphd@gmail.com>
2020-11-03 12:07:42 -0800

    conda activate

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ source $HOME/.bashrc
 
 #exec bash
 conda create -y -n narupa-dev "python>3.6"
-activate narupa-dev
+conda activate narupa-dev
 conda install -y -c omnia -c conda-forge -c irl narupa-server
 git clone https://gitlab.com/intangiblerealities/narupa-applications/isness.git
 cd isness
